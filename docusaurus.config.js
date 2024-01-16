dc8fff7f880cc3633507004ed98d6990542b293f
Armando Bueno <abueno@facephi.com>
2024-01-16 10:49:52 +0100

    Fix data urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "/sdk-mobile-documentation/",
+  url: "https://facephi.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/sdk-mobile-documentation/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
