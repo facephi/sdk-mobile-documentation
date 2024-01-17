371eb2df251959981b2f33c02bdff7368278f2a1
Armando Bueno <abueno@facephi.com>
2024-01-17 12:02:38 +0100

    Colors, images in main Screen updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         title: "SDK Mobile Documentation",
         logo: {
           alt: "SDK Mobile Logo",
-          src: "img/logo.svg",
+          src: "img/icon.png",
         },
         items: [
           {
