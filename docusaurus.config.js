661c5b77f7220e5581df4c4936e2b16a01a3d1f7
Armando Bueno <abueno@facephi.com>
2024-01-18 10:08:43 +0100

    icon updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config = {
   title: "SDK Mobile Documentation",
   tagline: "Facephi",
-  favicon: "img/favicon.ico",
+  favicon: "img/icon.png",
 
   staticDirectories: ["static"],
   // Set the production url of your site here
